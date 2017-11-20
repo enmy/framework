61171473b6e3a1c25a92a261fdaf2ed8d83588f3
enmy <enmyperez@gmail.com>
2017-11-19 22:29:04 -0300

    Create ODBCDatabase.php

---
+++ src/Database/ODBCDatabase.php
@@ -0,0 +1,81 @@
+<?php
+namespace Framework\Database;
+
+class ODBCDatabase extends Database
+{
+    public function query($sql, array $params = array())
+    {
+        if (! $this->link) {
+            $this->connect();
+        }
+
+        $this->id_query = odbc_exec($this->link, $sql);
+
+        if (! $this->id_query) {
+            throw new \Exception('Query inválida. '. $this->getError());
+        }
+
+        return $this;
+    }
+
+    public function connect()
+    {
+        $this->link = odbc_connect($this->db_connection->host, $this->db_connection->user, $this->db_connection->pass);
+
+        if (! $this->link) {
+            throw new \Exception('No es posible contectarse a la base de datos. '. $this->getError());
+        }
+
+        return $this;
+    }
+
+    public function fetchRow()
+    {
+        $this->isIdQuery();
+
+        return odbc_fetch_object($this->id_query);
+    }
+
+    public function fetchAllRow()
+    {
+        $all_row = array();
+
+        while ($row = $this->fetchRow()) {
+            $all_row[] = $row;var_dump($row);
+        }
+
+        return $all_row;
+    }
+
+    public function isIdQuery($throw_exception = true)
+    {
+        if (! isset($this->id_query)) {
+            if ($throw_exception) {
+                throw new \Exception('Query no especificado.');
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getError()
+    {
+        if (! $this->link) {
+            return '';
+        }
+
+        return '['. odbc_error($this->link). ': '. odbc_errormsg($this->link). ']';
+    }
+
+    public function disconnect(){}
+    public function getRow($query, Array $params = array()){}
+    public function fetchRowArray(){}
+    public function fetchAllRowArray(){}
+    public function fetchLastInsertId(){}
+    public function resultCount(){}
+    public function beginTransaction(){}
+    public function commitTransaction(){}
+    public function rollbackTransaction(){}
+}
